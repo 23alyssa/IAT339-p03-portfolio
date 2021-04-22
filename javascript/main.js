ade6e2a1a328695afa28af5cc7aec58490353144
alyssa <23alyssa23@gmail.com>
2021-04-21 18:42:34 -0700

    added hamburger menu
    made javascript folder and file 
    added accessible hamburger menu
    updated nav on all pages

---
+++ javascript/main.js
@@ -0,0 +1,74 @@
+'use strict';
+
+console.log("JavaScript is running.");
+
+var width = window.innerWidth;
+
+console.log("Window width: "+width+"px");
+
+var navToggle = document.querySelector("#nav-toggle");
+
+var navItems = document.querySelector("#nav-items");
+
+var navFirstItem = document.querySelector("#nav-items a");
+
+window.addEventListener("resize", function() {
+	width = window.innerWidth;
+
+
+    if (width < 610) {
+
+        console.log("Window width is less than 500px, collapsing menu.");
+
+        navToggle.classList.remove("hidden");
+
+        navToggle.setAttribute("aria-hidden", "false");
+
+
+        navItems.classList.add("hidden");
+
+        navItems.setAttribute("aria-hidden", "true");
+
+
+        navItems.setAttribute("aria-labelledby", "nav-toggle");
+
+    }
+
+    else {
+
+        navToggle.classList.add("hidden");
+        navToggle.setAttribute("aria-hidden", "true");
+        navItems.classList.remove("hidden");
+        navItems.setAttribute("aria-hidden", "false");
+
+    }
+});
+
+navToggle.addEventListener("click",
+	/*
+		Once the user has clicked on the element, we have - in this case - an anonymous (nameless) function that runs a series of statements for us.
+	*/
+	function() {
+		console.log("#nav-toggle has been clicked.");
+
+		if ( navItems.classList.contains("hidden") ) {
+			console.log("#nav-items are hidden, showing them now.");
+
+			navItems.classList.remove("hidden");
+
+			navItems.setAttribute("aria-hidden", "false");
+			navToggle.setAttribute("aria-expanded", "true");
+
+			navFirstItem.focus();
+		}
+
+		else {
+			console.log("#nav-items are showing, hiding them now.");
+
+			navItems.classList.add("hidden");
+
+			navItems.setAttribute("aria-hidden", "true");
+			navToggle.setAttribute("aria-expanded", "false");
+		}
+	}
+);
